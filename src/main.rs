0c81d7635a1f99f10515d4c9d4b521b497e37447
suyanlong <592938410@qq.com>
2018-03-21 16:47:17 +0800

    forserver complete code

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate hyper;
+
+use hyper::header::{ContentLength, ContentType};
+use hyper::server::{Http, Response, const_service, service_fn};
+
+static TEXT: &'static str = "";
+
+fn main() {
+    run();
+}
+
+fn run() -> Result<(), hyper::Error> {
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let hello = const_service(service_fn(|_req| {
+        Ok(Response::<hyper::Body>::new()
+            .with_header(ContentLength(TEXT.len() as u64))
+            .with_header(ContentType::plaintext())
+            .with_body(TEXT))
+    }));
+
+    let server = Http::new().bind(&addr, hello)?;
+    server.run()
+}
